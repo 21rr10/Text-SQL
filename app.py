9957fbacd1fc6df65b6fbc722e38fcd6944a2343
Ritesh Ranjan <122690733+21rr10@users.noreply.github.com>
2025-06-17 06:47:03 +0000

    update 2

--- app.py
+++ app.py
@@ -9,14 +9,27 @@ import google.generativeai as genai
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-
+def initialize_database(db_path):
+    if not os.path.exists(db_path):
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute("CREATE TABLE STUDENT(NAME VARCHAR(25), CLASS VARCHAR(25), SECTION VARCHAR(25), MARKS INT);")
+        cursor.execute("INSERT INTO STUDENT VALUES('Ritesh','Data Science','A',90);")
+        cursor.execute("INSERT INTO STUDENT VALUES('Ashish','DEVOPS','A',100);")
+        cursor.execute("INSERT INTO STUDENT VALUES('Rishit','Data Science','A',86);")
+        cursor.execute("INSERT INTO STUDENT VALUES('Ayush','DEVOPS','A',50);")
+        cursor.execute("INSERT INTO STUDENT VALUES('Ojha','DEVOPS','A',35);")
+        conn.commit()
+        conn.close()
+DB_PATH = "./student.db"
+initialize_database(DB_PATH)
 def get_gemini_response(question,prompt):
     model=genai.GenerativeModel('gemini-2.0-flash')
     response=model.generate_content([prompt[0],question])
     return response.text
 
 
-def read_sql_query(sql,db):
+def read_sql_query(sql,db=DB_PATH):
     conn=sqlite3.connect(db)
     cur=conn.cursor()
     cur.execute(sql)
@@ -54,7 +67,7 @@ submit = st.button("Ask the question")
 if submit:
     response = get_gemini_response(question, prompt)
     print(response)
-    data = read_sql_query(response, "student.db")
+    data = read_sql_query(response, DB_PATH)
     st.subheader("The Response is")
     for row in data:
         print(row)
